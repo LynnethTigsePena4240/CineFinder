371791fb2941193a9ca18b469acada3804cddb76
lena mukhtar <lena.mukhtar@gmail.com>
2025-07-30 23:18:44 -0400

    load a new working file with tailwind

---
+++ app/movieDetail/page.tsx
@@ -0,0 +1,12 @@
+"use client";
+
+export default function MovieDetailPage() {
+    return (
+        <div>
+        <div className ="text-white font-normal text-center m-7">
+            <h1>Movie Detail</h1>
+        </div>
+       
+        </div>
+    )
+}
